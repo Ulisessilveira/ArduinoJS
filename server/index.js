00e63bb94420275777e08c1cae13c11a2d3e54e8
Ulises Silveira <71781728+Ulisessilveira@users.noreply.github.com>
2023-05-11 11:14:53 -0600

    update ulisesssss

--- server/index.js
+++ server/index.js
@@ -1,3 +1,8 @@
+//ENCENDER EL SERVER
+/*
+OPCION 1 EN CMD npm start
+OPCION 2 en powershell entar con cd a server y luego escribir node index.js
+*/ 
 const express = require('express')
 const socketIo = require('socket.io')
 const http = require('http')
@@ -7,10 +12,13 @@ const server = http.createServer(app)
 const io = socketIo(server)
 
 //Ver si alguien se conecta al servidor
+//mandar cosos al arduino
 io.on('connection',(socket)=>{
+    //Se conecta una pagina web
     console.log('A new socket connected')
+    //manda cosas al arduino
     socket.on("message",(d)=>{
-        if(d == 'a') mySerial.write('a')
+        if(d == 'a') mySerial.write('a')//ESTO MANDA LO QUE TU QUIERES AL ARDUINO, NO OLVIDAR
         else if(d == 'b') mySerial.write('b')
     })
 })
@@ -44,15 +52,16 @@ let pot = 0;
 let ultrasonico = 0;
 let temp = 0;
 
+//Esto recibe los datos del arduino
 mySerial.on('data', function(data){
-    let values = data.toString().split("\r\n");
+    let values = data.toString().split("\r\n"); //Estos son los datos del arduino ta separados y metidos en un array
     pot = parseInt(values[0]);
     ultrasonico = parseInt(values[1]);
     temp = parseInt(values[2]);
-    io.emit('pot', pot);
+    io.emit('pot', pot);//esto emite lo de la consola al archivo HTML
     io.emit('ultrasonico', ultrasonico);
     io.emit('temp', temp);
-    console.log(`Pot: ${pot}, Ultrasonico: ${ultrasonico}, Temperatura: ${temp}`);
+    console.log(`Pot: ${pot}, Ultrasonico: ${ultrasonico}, Temperatura: ${temp}`);//Esto es para imprimir los valores en la consola
 });
 
 mySerial.on('error', function(err){
